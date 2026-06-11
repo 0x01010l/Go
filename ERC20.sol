227edbd87b9a990da7262fc92b9f374c6c54be7d
0x01010l <102048580+0x01010l@users.noreply.github.com>
2026-06-11 12:51:38 +0100

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -3,6 +3,8 @@
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/ERC20.sol)
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/ERC20.sol)
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/ERC20.sol)
+// OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/ERC20.sol)
+
 
 // goo
 pragma solidity ^0.8.0;
