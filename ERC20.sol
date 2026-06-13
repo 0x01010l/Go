ae2cabfe8633a8e566cbe5c0b2e2597b8df1ce4e
0x01010l <102048580+0x01010l@users.noreply.github.com>
2026-06-13 14:34:07 +0100

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/ERC20.sol)
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/ERC20.sol)
 // OpenZeppelin Contracts (last updated v4.9.0) (token/ERC20/ERC20.sol)
